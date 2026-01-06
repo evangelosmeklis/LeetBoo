5caf1c989437bde00f018e1520bbe1b0b50b9d93
Evangelos Meklis <vmeklis@hotmail.com>
2026-01-06 17:38:22 +0200

    Initial Commit

---
+++ LeetBoo/LeetBooApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeetBooApp.swift
+//  LeetBoo
+//
+//  Created by Evangelos Meklis on 6/1/26.
+//
+
+import SwiftUI
+
+@main
+struct LeetBooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
